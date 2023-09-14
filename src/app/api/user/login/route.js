0ae07495be9d94b1d60a533f0c9fd89726f5b10f
l3elugaBB <beluga.2B@gmail.com>
2023-09-14 15:14:56 +0700

    quiz3

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -4,15 +4,18 @@ import { DB, readDB } from "@/app/libs/DB";
 import { NextResponse } from "next/server";
 
 export const POST = async (request) => {
+  const body = await request.json();
+  const { username , password } = body;
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  const foundUser = DB.users.find((x)=> x.username===username && x.password===password)
+  if(!foundUser)
+  return NextResponse.json(
+    {
+      ok: false,
+      message: "Username or Password is incorrect",
+    },
+    { status: 400 }
+  );
 
   const token = "Replace this with token creation";
 
